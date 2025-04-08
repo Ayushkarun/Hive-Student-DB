51545eebddfe62af0e5896fb53ee36b53600fbeb
Ayushkarun <ayushkarun2580@gmail.com>
2025-04-08 15:56:52 +0530

    Updation

--- lib/home.dart
+++ lib/home.dart
@@ -57,7 +57,7 @@ class _HomeState extends State<Home> {
         _domainController.text.isEmpty) {
       ScaffoldMessenger.of(
         context,
-      ).showSnackBar(SnackBar(content: Text("Please fill both fields!")));
+      ).showSnackBar(SnackBar(content: Text("Please fill all fields!")));
       return;
     }
     final newItem = Mydb(
